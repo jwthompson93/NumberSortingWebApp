42d73cca5e845cfd0a805d2f7ec5b5eb8d9b8cf0
jwthompson93 <shadow17993@gmail.com>
2023-11-09 09:00:42 +0000

    Added Tests. Change folder structure

---
+++ NumberSortingTests/Algorithm/Sorting/QuicksortTests.cs
@@ -0,0 +1,35 @@
+using NumberSortingWebApp.Library.Algorithm.Sorting;
+
+namespace NumberSortingTests.Algorithm.Sorting
+{
+    public class QuicksortTests
+    {
+        private ISort<int> sortingAlgorithm;
+
+        [SetUp]
+        public void Setup()
+        {
+            sortingAlgorithm = new Quicksort();
+        }
+
+        [Test]
+        [TestCase(new int[] { 23, 24, 35, 40 }, new int[] { 23, 24, 35, 40 })]
+        [TestCase(new int[] { 40, 35, 24, 23 }, new int[] { 23, 24, 35, 40 })]
+        [TestCase(new int[] { 23, 35, 15, 643, 24, 9, 40 }, new int[] { 9, 15, 23, 24, 35, 40, 643 })]
+        public void Test_Quicksort_Ascending(int[] unsortedArray, int[] expectedArray)
+        {
+            int[] sortedArray = sortingAlgorithm.Sort(unsortedArray, SortDirection.Ascending);
+            Assert.That(sortedArray, Is.EqualTo(expectedArray));
+        }
+
+        [Test]
+        [TestCase(new int[] { 40, 35, 24, 23 }, new int[] { 40, 35, 24, 23 })]
+        [TestCase(new int[] { 23, 24, 35, 40 }, new int[] { 40, 35, 24, 23 })]
+        [TestCase(new int[] { 23, 35, 15, 643, 24, 9, 40 }, new int[] { 643, 40, 35, 24, 23, 15, 9 })]
+        public void Test_Quicksort_Descending(int[] unsortedArray, int[] expectedArray)
+        {
+            int[] sortedArray = sortingAlgorithm.Sort(unsortedArray, SortDirection.Descending);
+            Assert.That(sortedArray, Is.EqualTo(expectedArray));
+        }
+    }
+}
