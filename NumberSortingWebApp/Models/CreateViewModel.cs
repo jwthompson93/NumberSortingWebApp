3b25b0f85771daf2785f52281131ad79ba446ae7
jwthompson93 <shadow17993@gmail.com>
2023-11-10 12:41:38 +0000

    Added Annotations to the CreateViewModel values

--- NumberSortingWebApp/Models/CreateViewModel.cs
+++ NumberSortingWebApp/Models/CreateViewModel.cs
@@ -1,13 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using NumberSortingWebApp.Library.Alert;
+using System.ComponentModel.DataAnnotations;
 
 namespace NumberSortingWebApp.Models
 {
     public class CreateViewModel : PageModel
     {
         [BindProperty]
+        [Required]
+        [RegularExpression("[^\\d+,\\d+$]", ErrorMessage = "Values must be numeric and comma-separated")]
         public string NumbersInput { get; set; }
+
+
+        [BindProperty]
+        [Required]
         public int SortDirection { get; set; }
 
         public Alert Alert { get; set; }
